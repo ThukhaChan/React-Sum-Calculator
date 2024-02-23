b84fd9ee728aae1beb60fa93b47d426fc180d07a
ThuKhaCharmThar <thukhachanthar.dev@example.com>
2024-02-23 16:07:11 +0630

    add delete function

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { MinusIcon, PlusIcon, XMarkIcon } from "@heroicons/react/24/solid";
+import { MinusIcon, PlusIcon, TrashIcon, XMarkIcon } from "@heroicons/react/24/solid";
 import Button from "react-bootstrap/Button";
 
 function App() {
-  let [num1, setnum1] = useState(0);
-  let [num2, setnum2] = useState(0);
-  // let [sum, setSum] = useState();
-  // let [sub, setSub] = useState();
-  // let [div, setDiv] = useState();
-  // let [mul, setMul] = useState();
+  let [num1, setnum1] = useState();
+  let [num2, setnum2] = useState();
   let [result, setResult] = useState(null);
   let [operation, setOperation] = useState(null);
+
+  // ! Delete Function
+  let clearAll = () => {
+    setnum1('');
+    setnum2('');
+    setResult(null);
+    setOperation(null);
+  };
 
   // * Sum Function
   let sumNum = () => {
@@ -77,6 +81,7 @@ function App() {
           <input
             className="input"
             inputMode="decimal"
+            value={num1}
             required
             type="number"
             placeholder="Enter First Number"
@@ -89,6 +94,7 @@ function App() {
           <span className="fs-5">Second Number</span>
           <input
             type="number"
+            value={num2}
             inputMode="decimal"
             placeholder="Enter Second Number"
             required
@@ -99,28 +105,28 @@ function App() {
         </div>
         <div className="mt-3">
           <Button
-            className="btn me-lg-4"
+            className="btn me-lg-3 rounded-circle"
             style={{ backgroundColor: "#8cb0b6" }}
             onClick={sumNum}
           >
             <PlusIcon width={20} />
           </Button>
           <Button
-            className="btn me-lg-4"
+            className="btn me-lg-3 rounded-circle"
             style={{ backgroundColor: "#8cb0b6" }}
             onClick={subNum}
           >
             <MinusIcon width={20} />
           </Button>
           <Button
-            className="btn me-lg-4"
+            className="btn me-lg-3 rounded-circle"
             style={{ backgroundColor: "#8cb0b6" }}
             onClick={mulNum}
           >
             <XMarkIcon width={20} />
           </Button>
           <Button
-            className="btn me-lg-4"
+            className="btn me-lg-3 rounded-circle"
             style={{
               backgroundColor: "#8cb0b6",
               height: "38px",
@@ -128,7 +134,14 @@ function App() {
             }}
             onClick={divNum}
           >
-            {/* <span style={{ fontSize:'23px'}}> */}÷{/* </span> */}
+            ÷
+          </Button>
+          
+          <Button
+            className="btn btn-dark rounded-circle me-lg-3"
+            onClick={clearAll}
+          >
+            <TrashIcon width={20} />
           </Button>
           {operation && (
             <h5 className="mt-2">
